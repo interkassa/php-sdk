a280b712b1c4f2f122061b3ca1c2631f148c68d8
Denis Vikulov <d.vikulov@interkassa.com>
2022-03-17 13:52:32 +0200

    Updated generating signature helper

--- src/Helper/Signature.php
+++ src/Helper/Signature.php
@@ -11,11 +11,11 @@ class Signature
      *
      * @return string
      */
-    public function makeSignature(array $data, string $key, $algorithm = 'md5'): string
+    public static function makeSignature(array $data, string $key, $algorithm = 'md5'): string
     {
-        ksort($data, SORT_STRING);
-        array_push($data, $key);
-        $signString = implode(':', $data);
+        $data = self::sortByKeyRecursive($data);
+        $data[] = $key;
+        $signString = self::implodeRecursive(':', $data);
 
         return $algorithm === 'md5'
             ? base64_encode(md5($signString, true))
@@ -29,7 +29,7 @@ class Signature
      *
      * @return bool
      */
-    public function checkSignature(array $data, string $key, $algorithm = 'md5'): bool
+    public static function checkSignature(array $data, string $key, $algorithm = 'md5'): bool
     {
         if (!isset($data['ik_sign'])) {
             return true;
@@ -38,8 +38,29 @@ class Signature
         $signatureFromRequest = $data['ik_sign'];
         unset($data['ik_sign']);
 
-        $signature = $this->makeSignature($data, $key, $algorithm);
+        $signature = self::makeSignature($data, $key, $algorithm);
 
         return $signatureFromRequest === $signature;
+    }
+
+    private static function sortByKeyRecursive(array $array): array
+    {
+        ksort($array, SORT_STRING);
+        foreach ($array as $key => $value) {
+            if (is_array($value)) {
+                $array[$key] = self::sortByKeyRecursive($value);
+            }
+        }
+        return $array;
+    }
+
+    private static function implodeRecursive(string $separator, array $array): string
+    {
+        $result = '';
+        foreach ($array as $item) {
+            $result .= (is_array($item) ? self::implodeRecursive($separator, $item) : (string) $item) . $separator;
+        }
+
+        return substr($result, 0, -1 * strlen($separator));
     }
 }
